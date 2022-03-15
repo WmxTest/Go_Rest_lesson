b6f91d564aa286b074ca4a1176f2211810dee267
abmetko.a <abmetko.a@gmail.com>
2022-03-15 16:45:31 +0300

    added @CsvBindByPosition annotations into User.java

--- src/main/java/rest_assured/gorest_co_in/dto/User.java
+++ src/main/java/rest_assured/gorest_co_in/dto/User.java
@@ -1,19 +1,25 @@
 package rest_assured.gorest_co_in.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.opencsv.bean.CsvBindByPosition;
 import lombok.Data;
 
 @Data
 public class User {
 
+    @CsvBindByPosition(position = 0)
     @JsonProperty("email")
     private String mEmail;
+    @CsvBindByPosition(position = 1)
     @JsonProperty("gender")
     private String mGender;
+    @CsvBindByPosition(position = 2)
     @JsonProperty("id")
     private Integer mId;
+    @CsvBindByPosition(position = 3)
     @JsonProperty("name")
     private String mName;
+    @CsvBindByPosition(position = 4)
     @JsonProperty("status")
     private String mStatus;
 }
--- src/main/java/utils/CsvWorker.java
+++ src/main/java/utils/CsvWorker.java
@@ -46,4 +46,5 @@ public class CsvWorker {
             return null;
         }
     }
+
 }
