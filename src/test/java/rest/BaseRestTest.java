a997877ba283f5b3e8f7bcdae7932c6aa18f3dde
Stanislava_Masiukevich <m.stapol.m@gmail.com>
2022-03-16 10:24:44 +0300

    implemented rest.BaseRestTest.prepareDate and rest.BaseRestTest.getUser

--- src/test/java/rest/BaseRestTest.java
+++ src/test/java/rest/BaseRestTest.java
@@ -3,13 +3,30 @@ package rest;
 import org.junit.jupiter.api.*;
 import rest_assured.gorest_co_in.dto.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static rest_assured.gorest_co_in.UserService.getUsers;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseRestTest {
 
     protected static User user;
+    private static Queue<User> users;
 
     @BeforeAll
     public void beforeAll() {
+        prepareDate();
+    }
+
+    private static synchronized void prepareDate() {
+        if (users == null) {
+            users = new LinkedList<>(getUsers());
+        }
+    }
+
+    protected static synchronized User getUser() {
+        return users.poll();
     }
 }
