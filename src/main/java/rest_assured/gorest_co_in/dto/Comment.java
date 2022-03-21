4724abf6941c269f3454533c17550acb4857dc0f
KanarskayaRita <99012410+KanarskayaRita@users.noreply.github.com>
2022-03-21 10:57:03 +0300

    Refactoring

--- src/main/java/rest_assured/gorest_co_in/dto/Comment.java
+++ src/main/java/rest_assured/gorest_co_in/dto/Comment.java
@@ -3,6 +3,7 @@ package rest_assured.gorest_co_in.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.opencsv.bean.CsvBindByPosition;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -17,13 +18,18 @@ import lombok.Data;
 @Data
 public class Comment {
     @JsonProperty("id")
+    @CsvBindByPosition(position = 0)
     private Integer id;
     @JsonProperty("post_id")
+    @CsvBindByPosition(position = 1)
     private Integer postId;
     @JsonProperty("name")
+    @CsvBindByPosition(position = 2)
     private String name;
     @JsonProperty("email")
+    @CsvBindByPosition(position = 3)
     private String email;
     @JsonProperty("body")
+    @CsvBindByPosition(position = 4)
     private String body;
 }
--- src/main/java/utils/CsvWorker.java
+++ src/main/java/utils/CsvWorker.java
@@ -3,12 +3,14 @@ package utils;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import com.opencsv.exceptions.CsvValidationException;
+import rest_assured.gorest_co_in.UserService;
+import rest_assured.gorest_co_in.dto.User;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class CsvWorker {
@@ -24,6 +26,30 @@ public class CsvWorker {
         csvWriter.writeAll(body);
         csvWriter.close();
     }
+
+    public static <T> void writeData2(List<T> body, Writer writer) {
+        StatefulBeanToCsv<T> sbc = new StatefulBeanToCsvBuilder<T>(writer)
+                .withApplyQuotesToAll(false)
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .build();
+        try {
+            sbc.write(body);
+        } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<User> users = UserService.getUsers();
+        writeData2(users, new FileWriter(FILE_PATH));
+    }
+
 
     public static CSVReader getCsvReaderInstance() {
         if (csvReader == null) {
@@ -45,5 +71,32 @@ public class CsvWorker {
             e.printStackTrace();
             return null;
         }
+    }
+    public static <T> List<T> getObjectList(Class<T> cls, int skippedLines) {
+        List<T> list;
+        ColumnPositionMappingStrategy<T> mappingStrategy = new ColumnPositionMappingStrategy<>();
+        mappingStrategy.setType(cls);
+
+        Reader reader = null;
+        try {
+            reader = new FileReader(FILE_PATH);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        CsvToBean<T> cb = new CsvToBeanBuilder<T>(reader)
+                .withIgnoreQuotations(true)
+                .withIgnoreEmptyLine(true)
+                .withMappingStrategy(mappingStrategy)
+                .withSkipLines(skippedLines)
+                .build();
+
+        list = cb.parse();
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
